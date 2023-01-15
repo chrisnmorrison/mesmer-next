ad8530171fcd13af5fb52fbefceb2f370789c5da
Chris Morrison <chrnatmor@gmail.com>
2023-01-14 20:48:41 -0500

    remove posts for now

--- utils/posts.js
+++ utils/posts.js
@@ -1,20 +1,6 @@
 import matter from "gray-matter";
 import fs from "fs";
 
-export function getPostsFolders() {
-  // Get all posts folders located in `content/posts`
-  const postsFolders = fs
-    .readdirSync(`${process.cwd()}/content/posts`)
-    .map((folderName) => ({
-      directory: folderName,
-      filename: `${folderName}.md`,
-    }));
-
-  return postsFolders;
-}
-
-
-
 // Get day in format: Month day, Year. e.g. April 19, 2020
 function getFormattedDate(date) {
   const options = { year: "numeric", month: "long", day: "numeric" };
@@ -23,67 +9,81 @@ function getFormattedDate(date) {
   return formattedDate;
 }
 
-export function getSortedPosts() {
-  const postFolders = getPostsFolders();
 
-  const posts = postFolders
-    .map(({ filename, directory }) => {
-      // Get raw content from file
-      const markdownWithMetadata = fs
-        .readFileSync(`content/posts/${directory}/${filename}`)
-        .toString();
+// export function getPostsFolders() {
+//   // Get all posts folders located in `content/posts`
+//   const postsFolders = fs
+//     .readdirSync(`${process.cwd()}/content/posts`)
+//     .map((folderName) => ({
+//       directory: folderName,
+//       filename: `${folderName}.md`,
+//     }));
 
-      // Parse markdown, get frontmatter data, excerpt and content.
-      const { data, excerpt, content } = matter(markdownWithMetadata);
+//   return postsFolders;
+// }
 
-      const frontmatter = {
-        ...data,
-        date: getFormattedDate(data.date),
-      };
 
-      // Remove .md file extension from post name
-      const slug = filename.replace(".md", "");
+// export function getSortedPosts() {
+//   const postFolders = getPostsFolders();
 
-      return {
-        slug,
-        frontmatter,
-        excerpt,
-        content,
-      };
-    })
-    .sort(
-      (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
-    );
+//   const posts = postFolders
+//     .map(({ filename, directory }) => {
+//       // Get raw content from file
+//       const markdownWithMetadata = fs
+//         .readFileSync(`content/posts/${directory}/${filename}`)
+//         .toString();
 
-  return posts;
-}
+//       // Parse markdown, get frontmatter data, excerpt and content.
+//       const { data, excerpt, content } = matter(markdownWithMetadata);
 
-export function getPostsSlugs() {
-  const postFolders = getPostsFolders();
+//       const frontmatter = {
+//         ...data,
+//         date: getFormattedDate(data.date),
+//       };
 
-  const paths = postFolders.map(({ filename }) => ({
-    params: {
-      slug: filename.replace(".md", ""),
-    },
-  }));
+//       // Remove .md file extension from post name
+//       const slug = filename.replace(".md", "");
 
-  return paths;
-}
+//       return {
+//         slug,
+//         frontmatter,
+//         excerpt,
+//         content,
+//       };
+//     })
+//     .sort(
+//       (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+//     );
 
-export function getPostBySlug(slug) {
-  const posts = getSortedPosts();
+//   return posts;
+// }
 
-  const postIndex = posts.findIndex(({ slug: postSlug }) => postSlug === slug);
+// export function getPostsSlugs() {
+//   const postFolders = getPostsFolders();
 
-  const { frontmatter, content, excerpt } = posts[postIndex];
+//   const paths = postFolders.map(({ filename }) => ({
+//     params: {
+//       slug: filename.replace(".md", ""),
+//     },
+//   }));
 
-  const previousPost = posts[postIndex + 1];
-  const nextPost = posts[postIndex - 1];
+//   return paths;
+// }
 
-  return { frontmatter, post: { content, excerpt }, previousPost, nextPost };
-}
+// export function getPostBySlug(slug) {
+//   const posts = getSortedPosts();
 
-///////
+//   const postIndex = posts.findIndex(({ slug: postSlug }) => postSlug === slug);
+
+//   const { frontmatter, content, excerpt } = posts[postIndex];
+
+//   const previousPost = posts[postIndex + 1];
+//   const nextPost = posts[postIndex - 1];
+
+//   return { frontmatter, post: { content, excerpt }, previousPost, nextPost };
+// }
+
+///////////////////////////
 
 export function getPortfolioFolder() {
   // Get all posts folders located in `content/portfolio`
