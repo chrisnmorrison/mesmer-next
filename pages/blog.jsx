a5a72e3b0aa735176b1497088896dd6669b31d37
Chris Morrison <chrnatmor@gmail.com>
2023-03-22 14:08:45 -0400

    fix cms

---
+++ pages/blog.jsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Layout } from "../components/Layout";
+import { SEO } from "../components/Seo";
+import { getSortedPortfolio, getSortedBlog } from "@utils/posts";
+// import { generateRssPostsFeed } from "@utils/rss";
+import PageTitle from "@components/PageTitle";
+import styled from "styled-components";
+import Cta1 from "@components/Cta/Cta1";
+
+export default function Home({ blog }) {
+  return (
+      <Styles>
+      <PageTitle title="Portfolio" />
+      <SEO title="All of our favourite portfolio projects" />
+
+      <section className="section portfolio">
+        <div className="container">
+          <div className="row align-items-center">
+            {portfolio.map(({ frontmatter: { title, image }, slug }) => (
+              <div className="col-12 col-md-6" key={slug}>
+                <a
+                  href={`/portfolio/${encodeURIComponent(slug)}`}
+                  className="image-holder"
+                  data-aos="image-reveal"
+                >
+                  <div className="portfolio-item text-center">
+                  <div className={"image-container"}>
+                  <Image
+                        alt="portfolio item thumbnail"
+                        src={`/${image}`}
+                        fill
+                        className={"image img-fluid"}
+                      />
+              </div>
+                    <p className="sm product-name">{title}</p>
+                  </div>
+                </a>{" "}
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+<Cta1/>
+      
+      </Styles>
+  );
+}
+
+export async function getStaticProps() {
+  // generateRssPostsFeed();
+  const blog = getSortedBlog();
+
+  return {
+    props: {
+      blog,
+    },
+  };
+}
+
+export const Styles = styled.div`
+.image-holder{
+  display: block;
+overflow: hidden;
+margin-bottom: 38px;
+  }
+`
--- utils/posts.js
+++ utils/posts.js
@@ -9,80 +9,85 @@ function getFormattedDate(date) {
   return formattedDate;
 }
 
+///////////////////////////
+// BLOG
+///////////////////////////
 
-// export function getPostsFolders() {
-//   // Get all posts folders located in `content/posts`
-//   const postsFolders = fs
-//     .readdirSync(`${process.cwd()}/content/posts`)
-//     .map((folderName) => ({
-//       directory: folderName,
-//       filename: `${folderName}.md`,
-//     }));
+export function getBlogFolder() {
+  // Get all posts folders located in `content/portfolio`
+  const blogFolders = fs
+    .readdirSync(`${process.cwd()}/content/blog`)
+    .map((folderName) => ({
+      directory: folderName,
+      filename: `${folderName}.md`,
+    }));
 
-//   return postsFolders;
-// }
+  return blogFolders;
+}
+
+export function getSortedBlog() {
+  const postFolders = getBlogFolder();
+
+  const posts = postFolders
+    .map(({ filename, directory }) => {
+      // Get raw content from file
+      const markdownWithMetadata = fs
+        .readFileSync(`content/blog/${directory}/${filename}`)
+        .toString();
+
+      // Parse markdown, get frontmatter data, excerpt and content.
+      const { data, excerpt, content } = matter(markdownWithMetadata);
+
+      const frontmatter = {
+        ...data,
+        date: getFormattedDate(data.date),
+      };
+
+      // Remove .md file extension from post name
+      const slug = filename.replace(".md", "");
+
+      return {
+        slug,
+        frontmatter,
+        excerpt,
+        content,
+      };
+    })
+    .sort(
+      (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+    );
+
+  return posts;
+}
+
+export function getBlogSlugs() {
+  const blogFolders = getBlogFolder();
+
+  const paths = blogFolders.map(({ filename }) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+
+  return paths;
+}
+
+export function getBlogBySlug(slug) {
+  const posts = getSortedBlog();
+
+  const postIndex = posts.findIndex(({ slug: postSlug }) => postSlug === slug);
+
+  const { frontmatter, content, excerpt } = posts[postIndex];
+
+  const previousPost = posts[postIndex + 1];
+  const nextPost = posts[postIndex - 1];
+
+  return { frontmatter, post: { content, excerpt }, previousPost, nextPost };
+}
 
 
-// export function getSortedPosts() {
-//   const postFolders = getPostsFolders();
-
-//   const posts = postFolders
-//     .map(({ filename, directory }) => {
-//       // Get raw content from file
-//       const markdownWithMetadata = fs
-//         .readFileSync(`content/posts/${directory}/${filename}`)
-//         .toString();
-
-//       // Parse markdown, get frontmatter data, excerpt and content.
-//       const { data, excerpt, content } = matter(markdownWithMetadata);
-
-//       const frontmatter = {
-//         ...data,
-//         date: getFormattedDate(data.date),
-//       };
-
-//       // Remove .md file extension from post name
-//       const slug = filename.replace(".md", "");
-
-//       return {
-//         slug,
-//         frontmatter,
-//         excerpt,
-//         content,
-//       };
-//     })
-//     .sort(
-//       (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
-//     );
-
-//   return posts;
-// }
-
-// export function getPostsSlugs() {
-//   const postFolders = getPostsFolders();
-
-//   const paths = postFolders.map(({ filename }) => ({
-//     params: {
-//       slug: filename.replace(".md", ""),
-//     },
-//   }));
-
-//   return paths;
-// }
-
-// export function getPostBySlug(slug) {
-//   const posts = getSortedPosts();
-
-//   const postIndex = posts.findIndex(({ slug: postSlug }) => postSlug === slug);
-
-//   const { frontmatter, content, excerpt } = posts[postIndex];
-
-//   const previousPost = posts[postIndex + 1];
-//   const nextPost = posts[postIndex - 1];
-
-//   return { frontmatter, post: { content, excerpt }, previousPost, nextPost };
-// }
-
+///////////////////////////
+// PORTFOLIO
 ///////////////////////////
 
 export function getPortfolioFolder() {
